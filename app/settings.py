ac76287ac075180aa98c0ace27f2ca74df5a5336
unjeepero <fastrackerp@gmail.com>
2023-07-21 23:50:23 -0400

    ctec usach v1

--- app/settings.py
+++ app/settings.py
@@ -124,9 +124,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/media/'
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/images')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
